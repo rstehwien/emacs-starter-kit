0a3bf6ba3381a1bdc0f22f5809f89fd7cb66a97f
rstehwien <rstehwien@gmail.com>
2009-10-25 15:01:42 -0600

    add all clojure libs

--- universalmind.el
+++ universalmind.el
@@ -1,5 +1,11 @@
 (add-to-list 'load-path "~/.emacs.d/custom")
 (menu-bar-mode 1)
+(scroll-bar-mode 1)
 
 (clojure-slime-config)
-;(require 'quack)
+
+(defvar my-clojure-libs "~/src/clojure-libs")
+(when (file-directory-p my-clojure-libs)
+  (dolist (jar (directory-files my-clojure-libs t ".jar$"))
+    (add-to-list 'swank-clojure-classpath jar)))
+
